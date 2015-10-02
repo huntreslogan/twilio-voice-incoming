075cd91a327d9be6d45b74dc33a71592d43bdcb5
Jessica Hunt <huntres@me.com>
2015-10-02 12:36:15 -0700

    Added message recording functionality

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def hello_monkey():
 	resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
 
 	with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
-		g.say("To speak to a real monkey, press 1. Press any other key to start over.")
+		g.say("To speak to a real monkey, press 1. Press any other key to start over. Press 2 to record your own monkey howl. Press any other key to start over.")
 
 	return str(resp)
 
@@ -42,8 +42,26 @@ def handle_key():
 		resp.say('The call failed or the remote party hung up. Goodbye.')
 
 		return str(resp)
+	elif digit_pressed == "2":
+		resp = twilio.twiml.Response()
+		resp.say("Record your monkey howl after the tone.")
+		resp.record(maxLength="30", action="/handle-recording")
+		return str(resp)
 	else:
 		return redirect('/')
+
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+
+	recording_url = request.values.get("RecordingUrl", None)
+
+	resp = twilio.twiml.Response()
+
+	resp.say("Thanks for howling... take a listen to what you howled.")
+	resp.play(recording_url)
+	resp.say("Goodbye.")
+
+	return str(resp)
 
 if __name__ == "__main__":
 	app.run(debug=True)
