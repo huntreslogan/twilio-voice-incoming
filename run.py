30b289094932201bfe57e27e2cc348eb65bd3751
Jessica Hunt <huntres@me.com>
2015-10-02 12:19:44 -0700

    Added handle-key functionality

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect
+from __future__ import with_statement
 import twilio.twiml
 
 app = Flask(__name__)
@@ -24,7 +25,25 @@ def hello_monkey():
 
 	resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
 
+	with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+		g.say("To speak to a real monkey, press 1. Press any other key to start over.")
+
 	return str(resp)
+
+@app.route("/handle-key", methods=["GET", "POST"])
+def handle_key():
+
+	digit_pressed = request.values.get('Digit', None)
+	if digit_pressed == '1':
+		resp = twilio.twiml.Response()
+
+		resp.dial('+13105551212')
+
+		resp.say('The call failed or the remote party hung up. Goodbye.')
+
+		return str(resp)
+	else:
+		return redirect('/')
 
 if __name__ == "__main__":
 	app.run(debug=True)
