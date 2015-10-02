e7a2a4037c98b05afa15ce1d3fbced6ac28c9ce0
Jessica Hunt <huntres@me.com>
2015-10-02 11:57:41 -0700

    Greet callers by name

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
 
+callers = {
+	"+14158664966" : "Jessica",
+	"+17073383931" : "Donna"
+}
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_monkey():
+
+	from_number = request.values.get('From', None)
 	resp = twilio.twiml.Response()
-	resp.say("Hello Monkey")
+	if from_number in callers:
+		resp.say("Hello " + callers[from_number])
+	else:
+		resp.say("Hello Monkey")
 
 	return str(resp)
 
